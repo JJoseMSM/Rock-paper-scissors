dd892c9b5a92796094f3f9edcd515cc5b9534873
JoseMSM <nueljosema@gmail.com>
2024-04-22 15:29:23 -0600

    Itinerated the rps game into a for so it plays 5 times

--- script.js
+++ script.js
@@ -1,48 +1,51 @@
-
-
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random() * choices.length)];
 }
-
-let humanSelection = prompt('rock, paper, scissors');
 
 humanScore = 0;
 computerScore = 0;
 
 const computerSelection = getComputerChoice();
 
-function playRound(humanSelection, computerSelection) {
-    if(humanSelection == computerSelection) {
-        return `It's a tie you both chose ${humanSelection}`
-    } else if (humanSelection == 'rock' && computerSelection == 'scissors') {
-        humanScore++;
-        return `You win! ${humanSelection} beats ${computerSelection}`;
-    } else if (humanSelection == 'paper' && computerSelection == 'rock') {
-        humanScore++;
-        return `You win! ${humanSelection} beats ${computerSelection}`
-    } else if (humanSelection == 'scissors' && computerSelection == 'paper') {
-        humanScore++;
-        return `You win! ${humanSelection} beats ${computerSelection}`
-    } else if (computerSelection == 'rock' && humanSelection == 'scissors') {
-        computerScore++;
-        return `You lose, ${computerSelection} beats ${humanSelection}`
-    } else if (computerSelection == 'paper' && humanSelection == 'rock') {
-        computerScore++;
-        return `You lose, ${computerSelection} beats ${humanSelection}`
-    } else if (computerSelection == 'scissors' && humanSelection == 'paper') {
-        computerScore++;
-        return `You lose, ${computerSelection} beats ${humanSelection}`
+function playGame () {
+    for(i = 0; i < 5; i++){
+
+        let humanSelection = prompt('rock, paper, scissors');
+
+        function playRound(humanSelection, computerSelection) {
+            if(humanSelection == computerSelection) {
+                return `It's a tie you both chose ${humanSelection}`
+            } else if (humanSelection == 'rock' && computerSelection == 'scissors') {
+                humanScore++;
+                return `You win! ${humanSelection} beats ${computerSelection}`;
+            } else if (humanSelection == 'paper' && computerSelection == 'rock') {
+                humanScore++;
+                return `You win! ${humanSelection} beats ${computerSelection}`
+            } else if (humanSelection == 'scissors' && computerSelection == 'paper') {
+                humanScore++;
+                return `You win! ${humanSelection} beats ${computerSelection}`
+            } else if (computerSelection == 'rock' && humanSelection == 'scissors') {
+                computerScore++;
+                return `You lose, ${computerSelection} beats ${humanSelection}`
+            } else if (computerSelection == 'paper' && humanSelection == 'rock') {
+                computerScore++;
+                return `You lose, ${computerSelection} beats ${humanSelection}`
+            } else if (computerSelection == 'scissors' && humanSelection == 'paper') {
+                computerScore++;
+                return `You lose, ${computerSelection} beats ${humanSelection}`
+            }
+        }
+        console.log(humanSelection);
+        console.log(computerSelection);
+        console.log(playRound(humanSelection, computerSelection));
     }
 }
 
-console.log(humanSelection);
-console.log(computerSelection);
-console.log(playRound(humanSelection, computerSelection));
+playGame();
 
 if(humanScore > computerScore) {
     console.log(`You win! your final score is ${humanScore}`);
 } else {
     console.log(`You lose, computer final score is ${computerScore}`);
 }
-
